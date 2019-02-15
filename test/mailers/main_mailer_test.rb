6f87733810eb8c9ba98a88549bb0e54400f907c4
Lenmor Larroza Dimanalata <lenmor.larroza.dimanalata@autodesk.com>
2019-02-15 16:23:08 -0500

    css

--- test/mailers/main_mailer_test.rb
+++ test/mailers/main_mailer_test.rb
@@ -11,10 +11,10 @@ class MainMailerTest < ActionMailer::TestCase
 
     mail = MainMailer.notify_question_author(answer)
     assert_equal "New comment on your post", mail.subject
-    
+
     assert_equal [question.email], mail.to  # receiver
     assert_equal [answer.email], mail.from  # sender
-    
+
     # byebug
     assert_match answer.body, mail.body.encoded
   end
