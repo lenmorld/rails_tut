56c7b0b1372d04b47d0d8799252cf08e53d296f2
Lenmor Larroza Dimanalata <lenmor.larroza.dimanalata@autodesk.com>
2019-02-15 15:16:57 -0500

    email tests

---
+++ test/mailers/main_mailer_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+class MainMailerTest < ActionMailer::TestCase
+  test "notify_question_author" do
+
+    # fixtures
+    question = Question.create email: 'author@question.com', body: 'a test question'
+    answer = Answer.create email: 'author@answer.com', body: 'a test answer'
+
+    question.answers << answer
+
+    mail = MainMailer.notify_question_author(answer)
+    assert_equal "New comment on your post", mail.subject
+    
+    assert_equal [question.email], mail.to  # receiver
+    assert_equal [answer.email], mail.from  # sender
+    
+    # byebug
+    assert_match answer.body, mail.body.encoded
+  end
+
+end
